03c5ba661dbf78c535e9b82cee28d8a28a34a49b
HanhTran1994 <chenaxin888@gmail.com>
2026-03-02 21:44:21 +0700

    update

--- Day 5/index.js
+++ Day 5/index.js
@@ -1,9 +1,25 @@
-let time=10
-while (time >0){
-    time --;
-    if(time ===5){
-        console.log(`đã qua nửa thời gian!`);
-    }else{
-        console.log(`còn lại $(time) giây`);
-    }
-}console.log (`hết giờ`);
+// let time=30
+// const haflTime= Math.round(time /2);
+// while (time >0){
+//     time --;
+//     if(time ===haflTime){
+//         console.log(`đã qua nửa thời gian!`);
+//     }else{
+//         console.log(`còn lại ${time} giây`);
+//     }
+// }console.log (`hết giờ`);
+
+
+// const NUMBER_OF_ORDER=5
+// let total=0;
+
+// for (let i=0; i<NUMBER_OF_ORDER; i++ ){
+//  let price= Math.ceil(Math.random()*5000000)
+ 
+//   if (price<100000){
+//     price=100000;
+//   }
+//  total += price;
+//  console.log(`tổng tiền sau đơn hàng thứ $(i+1) là: ${total.toLocaleString()}VND`)
+// }
+// // console.log(`tổng tiền phải trả ${total.toLocaleString()}VND`);
