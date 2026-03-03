e7686d941829707e15e77b8cec99aba3a7386c24
HanhTran1994 <chenaxin888@gmail.com>
2026-03-03 09:28:06 +0700

    update

--- Day 6/index.js
+++ Day 6/index.js
@@ -5,6 +5,6 @@
 
 
 
-function introduce(TranTinHanh,30,HCMcity){
-    console.log("xin chao, toi ten la Tran Tin Hanh, nam nay 31 tuoi , toi sinh song o TP Ho Chi Minh");
-}
+// function introduce(TranTinHanh,30,HCMcity){
+//     console.log("xin chao, toi ten la Tran Tin Hanh, nam nay 31 tuoi , toi sinh song o TP Ho Chi Minh");
+// }
