2ecc90eac5c8b9d97283eefcbebc14656295064e
HanhTran1994 <chenaxin888@gmail.com>
2026-03-04 21:57:36 +0700

    update

--- Day 6/index.js
+++ Day 6/index.js
@@ -1,11 +1,44 @@
-// function printName(Hanh){
-//  console.log(`Hello , Hanh`);
+// function printName(name){
+//  console.log(name);
 // }
 // printName(`Hello , Hanh`);
 
 
 
-function introduce(name,age,address){
-    console.log("name,age,address");
-}
-introduce(`xin chao, toi ten la Tran Tin Hanh, nam nay 31 tuoi , toi sinh song o TP Ho Chi Minh`);
+// function introduce(name,age,address){
+//     console.log(name,age,address);
+// }
+// introduce(`xin chao, toi ten la Tran Tin Hanh, nam nay 31 tuoi , toi sinh song o TP Ho Chi Minh`);
+
+
+// function calculatePytago(canhGocVuong1,canhGocVuong2){
+//    let Pytago=
+//    return Pytago;
+// }
+// console.log();
+
+
+// const calculator= (number1,number2,operator) => {
+//     switch(operator){
+//         case "+":{
+//          return number1 + number2 
+//         }
+//         case "-":{
+//          return number1 - number2  
+//         }
+//         case"*" :{
+//          return number1 * number2
+//         } 
+//         case"/" :{
+//         if (number2 ===0 ){
+//             return" khong the chia cho 0"
+//         }
+         
+//          return number1 / number2
+//     }
+//     default :{
+//       return "phep tinh khong hop le"
+//     }
+// }
+// }
+//  console.log(calculator(3 ,0 ,"/"))
