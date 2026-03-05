983ba7c12d5c73bbee0b03e06bd53533a42d7248
HanhTran1994 <chenaxin888@gmail.com>
2026-03-05 20:34:11 +0700

    day 8

--- Day 7/index.js
+++ Day 7/index.js
@@ -4,3 +4,19 @@
 // console.log(carts[0]);
 // console.log(carts[carts.length-1]);
 
+const friendName =['Linh','Nam','Thanh'];
+friendName.push('Dũng')
+friendName.unshift("Nhi");
+friendName.pop()
+friendName.splice(1,0,"Hải")
+
+
+console.log(friendName); 
+console.log(friendName.length);
+console.log(friendName[0]);
+
+let i=0
+for(let i=0; i < friendName.length ;i++){
+    console.log(friendName[i])
+}
+
