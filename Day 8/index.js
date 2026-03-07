0bb0acf8f33c1dcbd701612e4dd80efc1e547abc
HanhTran1994 <chenaxin888@gmail.com>
2026-03-07 12:07:26 +0700

    Day 9

--- Day 8/index.js
+++ Day 8/index.js
@@ -10,3 +10,27 @@
 //         console.log(number);
 //     }
 // });
+
+const scores= [4.5,8,9,3.8,6,7.8,9.8,6.6,4.9,7];
+
+// const hasBelowAverage= scores.some(scores=>scores<5)
+//     console.log(hasBelowAverage,"dưới trung bình");
+// const allPassed= scores.every(scores=>scores>=5)
+//     console.log(allPassed,"toàn bộ passed")
+
+// const newScores= scores.map((score)=>{
+//     if (score ===10 ){
+//     return score;
+//      }else{
+//      return score+1;
+//      }
+// })
+//  console.log(newScores);
+     
+// const numbers=[5,7,4,8,9,1,3];
+
+// const result =numbers.reduce((prevValue,curValue)=>{
+//   console.log(`${prevValue}-${curValue}`);
+//   return prevValue + curValue;
+// },0);
+// console.log(result);
