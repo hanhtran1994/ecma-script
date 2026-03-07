d03da8fb34b8d70ab77b2f70212912e4723a2d27
HanhTran1994 <chenaxin888@gmail.com>
2026-03-07 09:40:05 +0700

    day 8

---
+++ Day 8/index.js
@@ -0,0 +1,12 @@
+// const provinces= ['Ho Chi Minh', 'Ha Noi', 'Đa Lat','Đa Nang','Binh Duong','Long An' ];
+// provinces.forEach( (province,index)=>{
+//  console.log (`${province}-${index}`);
+// })
+
+// const numbers = [3, 8, 5, 12, 7, 10, 15, 20, 9, 6];
+
+// numbers.forEach(number => {
+//     if (number % 2 === 0) {
+//         console.log(number);
+//     }
+// });
