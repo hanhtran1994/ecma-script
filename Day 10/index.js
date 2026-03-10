886e9de63c810d4f7afb8d3a20028a7e6b2e8a9f
HanhTran1994 <chenaxin888@gmail.com>
2026-03-10 19:34:11 +0700

    day 11

--- Day 10/index.js
+++ Day 10/index.js
@@ -1,1 +1,31 @@
-let name =prompt ("nhập vào tên của bạn:");
+const employees=[]
+
+const prompt = require("prompt-sync")();
+let number =Number(prompt("Nhập số lượng nhân viên mà bạn muốn tạo:"));
+
+let id =1 ;
+
+for (let i=0; i<number;i++){
+    const name = prompt ("Nhập vào tên nhân viên:" );
+    const email= prompt ("Nhập vào email nhân viên:" );
+    const phoneNumber= prompt ("Nhập vào số điện thoại nhân viên:" );
+    const gender= prompt ("Nhập vào giới tính nhân viên:" );
+    const position= prompt ("Nhập vào vị trí nhân viên:" );
+    const salary= prompt ("Nhập vào mức lương nhân viên:" );
+
+    employees.push({
+
+    
+        id,
+        name,
+        email,
+        phoneNumber,
+        gender,
+        position,
+        salary,
+
+     });
+     id++;
+     
+}
+console.log(employees);
