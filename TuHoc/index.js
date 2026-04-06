f6858655d104e54f260601a1e748ed805b769e4b
HanhTran1994 <chenaxin888@gmail.com>
2026-04-06 16:42:42 +0700

    webcafe

---
+++ TuHoc/index.js
@@ -0,0 +1,24 @@
+// let number =5;
+// let isEven = number %2===0 ;
+// isEven === true
+// ? console.log (`${number} là sô chẵn`)
+// : console.log (`${number} là số lẻ` );
+
+
+// let num = 6
+//  if  (num %2 ===0 ){
+//    console.log(`${num} la so chan`)
+//  }else {
+//     console.log (`${num}la so le`);
+//  }
+
+// let number = 0
+// if(number >0) {
+//    console.log(`${number} là số dương`)
+// }else if (number <0){
+//   console.log (`${number} là số âm`);
+// }else {
+//   console.log ( `${number} =0`);
+// }
+  
+
